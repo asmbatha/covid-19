9876c8b77e6ea0a30e3dd84edc66307a9e97142c
Ayanda Mbatha <asmbatha.13@gmail.com>
2022-07-05 23:11:50 +0200

    set up db, server and client

--- start.sh
+++ start.sh
@@ -2,6 +2,10 @@
 
 docker pull postgres
 docker-compose up -d
-sleep 5
-cd react-web-client
+
+cd service
+python3 -m pip install -r requirements.txt
+cd ../react-web-client
+npm i
+sleep 20
 npm run start:all
