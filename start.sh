1908eab51df40e1c4c64dfbc6c210b2ea55577cc
Ayanda Mbatha <asmbatha.13@gmail.com>
2022-07-07 18:12:17 +0000

    Automatically remove the container and it’s associated file system upon exit

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 docker pull postgres
-docker-compose up -d
+docker run --rm --name pg-docker -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres
 
 cd service
 python3 -m pip install -r requirements.txt
 cd ../react-web-client
 npm i
-sleep 20
 npm run start:all
